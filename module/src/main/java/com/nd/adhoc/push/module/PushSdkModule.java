cbdc409a5913900ccb31f5d65b707f299eff1d90
赖碧威 <880723@nd.com>
2019-01-09 15:20:10 +0800

    1 防止取mac时死循环，但mac可能为空 2 去除对无线网卡状态的侦听 3 10秒尝试重连一次 4 增加上行消息接口
    Change-Id: Ib5c7f0083a1534cc2963eb62b5004b04986ea57e

--- module/src/main/java/com/nd/adhoc/push/module/PushSdkModule.java
+++ module/src/main/java/com/nd/adhoc/push/module/PushSdkModule.java
@@ -146,6 +146,21 @@ public class PushSdkModule {
     }
 
     /**
+     * 发送上行消息
+     *
+     * @param msgid 消息ID
+     * @param ttlSeconds 过期时间
+     * @param contentType 消息类型
+     * @param content 消息内容
+     * @return 0 成功
+     *         非0 失败
+     */
+    @SuppressLint("DefaultLocale")
+    public int sendUpStreamMsg(String msgid, long ttlSeconds, String contentType, String content) {
+        return libpushclient.native_sendUpStreamMsg(msgid, ttlSeconds, contentType, content);
+    }
+
+    /**
      * 开始接收Push消息
      *
      * @param context      context
